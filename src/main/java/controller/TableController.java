3c49b55719e01c80a0df515fb1281d2349e4e657
SavindaJayasekara <thantrige32@gmail.com>
2023-11-13 14:05:45 +0530

    add components

--- src/main/java/controller/TableController.java
+++ src/main/java/controller/TableController.java
@@ -1,4 +1,52 @@
 package controller;
 
+import DB.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TableController {
+
+    public TableView tblCustomer;
+    public TableColumn colId;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colName;
+
+    @FXML
+    void initialize(){
+        ObservableList<CustomerTM> list = FXCollections.observableArrayList();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement("select * from customer");
+            ResultSet resultSet = pstm.executeQuery();
+
+            while (resultSet.next()){
+                list.add(new CustomerTM(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getDouble(4)
+                ));
+            }
+
+            tblCustomer.setItems(list);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+    }
 }
